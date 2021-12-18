923030969dcee64ae802b39690c072e27e8ab380
truyem789 <85695267+truyem789@users.noreply.github.com>
2021-12-18 16:12:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ bot.loadPlugin(cmd)
 
 bot.on('login',function(){
 	console.log("Logged In")
-	bot.chat("/gamemode 1");
 	bot.chat("HI im GOD");
 	bot.chat("You can't defeat me");
 	bot.chat("Muhahaha");
+	bot.chat("/gamemode 1");
 });
 
 bot.on('time', function(time) {
