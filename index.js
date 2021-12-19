701631a044abe0cc72589e339ba0ef24b93e35b3
truyem789 <85695267+truyem789@users.noreply.github.com>
2021-12-19 14:16:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var connected = 0;
 var actions = [ 'forward', 'back', 'left', 'right']
 var lastaction;
 var pi = 3.14159;
-var moveinterval = 4; // 2 second movement interval
-var maxrandom = 10; // 0-5 seconds added to movement interval (randomly)
+var moveinterval = 2; // 2 second movement interval
+var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
